ad0b1e983256e68b9c0e7be150a14bdc95fd928d
Horatio Thomas <horatiothomas@gmail.com>
2025-10-06 17:24:13 +0700

    Cleanup AllAccountsWithHoldingsMessageMapper

--- ktor/src/main/kotlin/io/tenetinc/knance/ktor/api/mappers/AllAccountsWithHoldingsMessageMapper.kt
+++ ktor/src/main/kotlin/io/tenetinc/knance/ktor/api/mappers/AllAccountsWithHoldingsMessageMapper.kt
@@ -1,23 +1,26 @@
 package io.tenetinc.knance.ktor.api.mappers
 
+import io.tenetinc.knance.domain.repository.AllAccountsWithHoldingsBulkQuoteMessage
 import io.tenetinc.knance.domain.repository.AllAccountsWithHoldingsMessage
+import io.tenetinc.knance.domain.repository.AllAccountsWithHoldingsWithoutMarketDataMessage
+import io.tenetinc.knance.domain.repository.AllAccountsWithMarketDataHoldingsMessage
 
 typealias SerializableAllAccountsWithHoldingsMessage = io.tenetinc.knance.common.api.model.AllAccountsWithHoldingsMessage
 
 fun AllAccountsWithHoldingsMessage.toSerializable(): SerializableAllAccountsWithHoldingsMessage {
     return when (this) {
-        is io.tenetinc.knance.domain.repository.AllAccountsWithHoldingsWithoutMarketDataMessage -> 
-            io.tenetinc.knance.common.api.model.AllAccountsWithHoldingsMessage(
+        is AllAccountsWithHoldingsWithoutMarketDataMessage ->
+            SerializableAllAccountsWithHoldingsMessage(
                 type = "AllAccountsWithHoldingsWithoutMarketDataMessage",
                 accounts = accounts.map { it.toSerializable() }
             )
-        is io.tenetinc.knance.domain.repository.AllAccountsWithHoldingsBulkQuoteMessage -> 
-            io.tenetinc.knance.common.api.model.AllAccountsWithHoldingsMessage(
+        is AllAccountsWithHoldingsBulkQuoteMessage ->
+          SerializableAllAccountsWithHoldingsMessage(
                 type = "AllAccountsWithHoldingsBulkQuoteMessage",
                 bulkQuoteMessage = bulkQuoteMessage.toSerializable()
             )
-        is io.tenetinc.knance.domain.repository.AllAccountsWithMarketDataHoldingsMessage -> 
-            io.tenetinc.knance.common.api.model.AllAccountsWithHoldingsMessage(
+        is AllAccountsWithMarketDataHoldingsMessage ->
+          SerializableAllAccountsWithHoldingsMessage(
                 type = "AllAccountsWithMarketDataHoldingsMessage",
                 accounts = accounts.map { it.toSerializable() }
             )
