5ca3ff682b78d3f35ce460c0ca5d34727be365c3
Horatio Thomas <horatiothomas@gmail.com>
2025-10-12 17:39:20 +0700

    Create the response model

---
+++ market-data/alpha-vantage/src/main/kotlin/io/tenetinc/knance/client/model/overview/CompanyOverviewResponse.kt
@@ -0,0 +1,8 @@
+package io.tenetinc.finance.alphavantage.io.tenetinc.knance.client.model.overview
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CompanyOverviewResponse(
+    val companyOverview: CompanyOverview
+)
